65b05991702cd5aa3ea2be30eab9186044901b7e
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-05-20 22:42:29 +0600

    user registration completed

--- src/auth/register/register.controller.ts
+++ src/auth/register/register.controller.ts
@@ -4,6 +4,12 @@ import { registerServices } from "./register.services";
 const registerUser=async(req:Request,res:Response)=>{
     try {
         const result =await registerServices.createUserInDb(req.body)
+        delete result.rows[0].password
+        res.status(201).json({
+            success:true,
+            message:"User registered successfully",
+            data:result.rows[0]
+        })
     } catch (error) {
         console.log(error,'error in sign up controller')
     }
--- src/auth/register/register.services.ts
+++ src/auth/register/register.services.ts
@@ -3,17 +3,13 @@ import { pool } from "../../database"
 import type { IRegisterUser } from "./register.interface"
 
 const createUserInDb=async(payload:IRegisterUser)=>{
-    try {
-        const{name,email,password,role}=payload
+  const{name,email,password,role="contributor"}=payload
         const hashPassword=await bcrypt.hash(password,10)
         const result=await pool.query(`
             INSERT INTO users(name,email,password,role) VALUES($1,$2,$3,$4)
             RETURNING * 
         `,[name,email,hashPassword,role])
         return result
-    } catch (error) {
-        console.log(error,'error while createing user in db')
-    }
 }
 
 export const registerServices={
